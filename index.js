1cb8bd940d5c99f75a8d96f0d40bbab31fb23bfe
TatianaLoskutova <tanya.loskutova333@gmail.com>
2023-05-06 17:02:00 +0300

    Html and css added

---
+++ index.js
@@ -0,0 +1,4 @@
+let buttonPlus = document.getElementById('buttonPlus');
+let buttonMinus = document.getElementById('buttonMinus');
+let buttonMultiply = document.getElementById('buttonMultiply');
+let buttonDivide = document.getElementById('buttonDivide');
